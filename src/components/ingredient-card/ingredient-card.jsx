3a31095a18dfa394b3aa2aae8c07555b62eecc9d
Ivan Sizov <vsizov63@gmail.com>
2022-09-26 09:56:41 +0300

    feat: add IngredientDetails component

--- src/components/ingredient-card/ingredient-card.jsx
+++ src/components/ingredient-card/ingredient-card.jsx
@@ -1,38 +1,50 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { Counter } from '@ya.praktikum/react-developer-burger-ui-components'
 
-import { Price } from 'components'
+import { Price, Modal, IngredientDetails } from 'components'
 import { ingredient } from '../../utils/prop-types'
 
 import ingredientCardStyles from './ingredient-card.module.scss'
 
 const IngredientCard = ({ ingredient, onClick }) => {
+  const [isOpen, setOpen] = useState(false)
+
   return (
-    <figure
-      className={ingredientCardStyles.figure}
-      onClick={onClick}
-    >
-      <img
-        src={ingredient.image}
-        alt={ingredient.name}
-        className={`mr-4 ml-4 mb-1 ${ingredientCardStyles.image}`}
-      />
-      {ingredient.count > 0 && (
-        <Counter
-          size='default'
-          count={ingredient.count}
+    <>
+      <figure className={ingredientCardStyles.figure}>
+        <img
+          src={ingredient.image}
+          alt={ingredient.name}
+          className={`mr-4 ml-4 mb-1 ${ingredientCardStyles.image}`}
+          onClick={() => setOpen(true)}
         />
-      )}
-      <figcaption className={ingredientCardStyles.caption}>
-        <Price value={ingredient.price} />
-        <p
-          className={`text text_type_main-default text_color_primary ${ingredientCardStyles.paragraph}`}
+        {ingredient.count > 0 && (
+          <Counter
+            size='default'
+            count={ingredient.count}
+          />
+        )}
+        <figcaption
+          className={ingredientCardStyles.caption}
+          onClick={onClick}
         >
-          {ingredient.name}
-        </p>
-      </figcaption>
-    </figure>
+          <Price value={ingredient.price} />
+          <p
+            className={`text text_type_main-default text_color_primary ${ingredientCardStyles.paragraph}`}
+          >
+            {ingredient.name}
+          </p>
+        </figcaption>
+      </figure>
+      <Modal
+        isOpen={isOpen}
+        setOpen={setOpen}
+      >
+        <IngredientDetails {...ingredient} />
+      </Modal>
+    </>
   )
 }
 
---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,100 @@
+import { ingredient as ingredientType } from 'utils/prop-types'
+
+import ingredientDetailsStyles from './ingredient-details.module.scss'
+
+const IngredientDetails = ingredient => {
+  const { image_large, name, fat, calories, proteins, carbohydrates } =
+    ingredient
+
+  return (
+    <div className={`${ingredientDetailsStyles.container} mt-10 mb-15`}>
+      <h2 className='text text_type_main-large text_color_primary'>
+        Детали ингредиента
+      </h2>
+      <img
+        src={image_large}
+        alt='изображение ингредиента'
+        className='mb-4'
+      />
+      <h3 className='text text_type_main-medium text_color_primary mb-8'>
+        {name}
+      </h3>
+      <div className={ingredientDetailsStyles.specs}>
+        <div className={ingredientDetailsStyles.spec}>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            Калории,
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            ккал
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_digits-default text_color_inactive`}
+          >
+            {calories}
+          </span>
+        </div>
+        <div className={ingredientDetailsStyles.spec}>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            Белки,
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            г
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_digits-default text_color_inactive`}
+          >
+            {proteins}
+          </span>
+        </div>
+        <div className={ingredientDetailsStyles.spec}>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            Жиры,
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            г
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_digits-default text_color_inactive`}
+          >
+            {fat}
+          </span>
+        </div>
+        <div className={ingredientDetailsStyles.spec}>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            Углеводы,
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_main-default text_color_inactive`}
+          >
+            г
+          </span>
+          <span
+            className={`${ingredientDetailsStyles.item} text text_type_digits-default text_color_inactive`}
+          >
+            {carbohydrates}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+IngredientDetails.propTypes = {
+  ingredient: ingredientType.isRequired,
+}
+
+export default IngredientDetails
