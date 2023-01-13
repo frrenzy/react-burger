3b25c503738d49d715ba3f433e433207549e8ae4
Ivan Sizov <vsizov63@gmail.com>
2023-01-14 02:08:08 +0300

    fix: review #1 - add wsActions parameter to WS middleware

--- src/redux/middleware/socketMiddleware.ts
+++ src/redux/middleware/socketMiddleware.ts
@@ -1,58 +1,46 @@
 import { Middleware } from 'redux'
-import {
-  WSConnectionClosedAction,
-  WSConnectionErrorAction,
-  WSConnectionSuccessAction,
-  WSGetMessageAction,
-} from 'services/actions/feed'
-import { WS_CONNECTION_END, WS_CONNECTION_START } from 'services/constants/feed'
 import { AppDispatch, RootState, TAppActions } from 'services/types'
+
+import { IWSActions } from '../store'
 import { IOrderWSRaw } from 'services/types/data'
 
-export const socketMiddleware: Middleware<
-  {},
-  RootState,
-  AppDispatch
-> = store => {
-  let socket: WebSocket | null = null
+export const socketMiddleware =
+  (wsActions: IWSActions): Middleware<{}, RootState, AppDispatch> =>
+  store => {
+    let socket: WebSocket | null = null
+    const { wsInit, wsClose, onOpen, onClose, onError, onMessage } = wsActions
 
-  return next => (action: TAppActions) => {
-    const { dispatch } = store
-    const { type } = action
+    return next => (action: TAppActions) => {
+      const { dispatch } = store
+      const { type } = action
 
-    if (type === WS_CONNECTION_START) {
-      const { url } = action
-      if (!socket || socket.readyState === 3) {
-        socket = new WebSocket(`${url}`)
-      } else if (socket.url !== url) {
-        socket.close()
-        socket = new WebSocket(`${url}`)
+      if (type === wsInit('').type) {
+        const { url } = action
+        if (!socket || socket.readyState === 3) {
+          socket = new WebSocket(url)
+        } else if (socket.url !== url) {
+          socket.close()
+          socket = new WebSocket(url)
+        }
+      } else if (type === wsClose().type) {
+        socket?.close()
       }
-    } else if (type === WS_CONNECTION_END) {
-      socket?.close()
+      if (socket) {
+        socket.addEventListener('open', event => dispatch(onOpen(event)))
+
+        socket.addEventListener('close', event => dispatch(onClose(event)))
+
+        socket.addEventListener('error', event => dispatch(onError(event)))
+
+        socket.addEventListener('message', (event: MessageEvent<string>) => {
+          const { data } = event
+          const parsedData: IOrderWSRaw = JSON.parse(data)
+          const { success, ...restParsedData } = parsedData
+
+          dispatch(onMessage(restParsedData))
+        })
+      }
+
+      next(action)
     }
-    if (socket) {
-      socket.addEventListener('open', event => {
-        dispatch(WSConnectionSuccessAction(event))
-      })
-
-      socket.addEventListener('error', event => {
-        dispatch(WSConnectionErrorAction(event))
-      })
-
-      socket.addEventListener('message', (event: MessageEvent<string>) => {
-        const { data } = event
-        const parsedData: IOrderWSRaw = JSON.parse(data)
-        const { success, ...restParsedData } = parsedData
-
-        dispatch(WSGetMessageAction(restParsedData))
-      })
-
-      socket.addEventListener('close', event => {
-        dispatch(WSConnectionClosedAction(event))
-      })
-    }
-
-    next(action)
   }
-}
